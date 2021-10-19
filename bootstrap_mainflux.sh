a531fdc4bb0338eff06e74c0fa6c7668d6828410
Jan <philkry@gmx.de>
2021-10-19 20:25:42 +0200

    changed env path

--- bootstrap_mainflux.sh
+++ bootstrap_mainflux.sh
@@ -5,7 +5,7 @@ MAINFLUX_USERS_HOST="${MAINFLUX_USERS_HOST:-http://mainflux-users.mf.svc.cluster
 #MAINFLUX_USERS_HOST="https://mainflux.k8s.h.kryno.de"
 MAINFLUX_BOOTSTRAP_HOST="${MAINFLUX_BOOTSTRAP_HOST:-http://mainflux-bootstrap.mf.svc.cluster.local:8182}"
 #MAINFLUX_BOOTSTRAP_HOST="https://mainflux.k8s.h.kryno.de"
-ENV_FILE="${ENV_FILE:-/data/.env}"
+ENV_FILE="${ENV_FILE:-/data/env/.env}"
 #ENV_FILE="/data/.env"
 BINARY="/data/mainflux-cli"
 
