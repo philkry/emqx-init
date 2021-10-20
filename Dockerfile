1b2f83309e2075708b9411c7d04ce20e78e00f8d
Jan <philkry@gmx.de>
2021-10-20 12:07:14 +0200

    addind logging and try-catch

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ FROM alpine
 RUN apk --no-cache add bash curl jq \
     && mkdir /data
 COPY bootstrap_mainflux.sh /data/
+COPY lib /data/lib/
 COPY --from=cli /exe /data/mainflux-cli
 ENTRYPOINT [ "/data/bootstrap_mainflux.sh" ]
 
--- bootstrap_mainflux.sh
+++ bootstrap_mainflux.sh
@@ -1,64 +1,83 @@
-#!/bin/bash
-MAINFLUX_THINGS_HOST="${MAINFLUX_THINGS_HOST:-http://mainflux-things.mf.svc.cluster.local:8182}"
-#MAINFLUX_THINGS_HOST="https://mainflux.k8s.h.kryno.de"
-MAINFLUX_USERS_HOST="${MAINFLUX_USERS_HOST:-http://mainflux-users.mf.svc.cluster.local:8180}"
-#MAINFLUX_USERS_HOST="https://mainflux.k8s.h.kryno.de"
-MAINFLUX_BOOTSTRAP_HOST="${MAINFLUX_BOOTSTRAP_HOST:-http://mainflux-bootstrap.mf.svc.cluster.local:8182}"
-#MAINFLUX_BOOTSTRAP_HOST="https://mainflux.k8s.h.kryno.de"
-ENV_FILE="${ENV_FILE:-/data/env/.env}"
-#ENV_FILE="/data/.env"
-BINARY="/data/mainflux-cli"
+#!/usr/bin/env bash
+source "$( cd "${BASH_SOURCE[0]%/*}" && pwd )/lib/oo-bootstrap.sh"
+import util/log util/exception util/tryCatch
 
-TOKEN=$($BINARY -t $MAINFLUX_THINGS_HOST -u $MAINFLUX_USERS_HOST -r users token $MAINFLUX_USER $MAINFLUX_PASSWORD)
-NODE_RED_THINGS=$($BINARY -t $MAINFLUX_THINGS_HOST -u $MAINFLUX_USERS_HOST -r things get all -n node-red $TOKEN)
-NODE_RED_COUNT=$(echo $NODE_RED_THINGS | jq '.total')
-CHANNELS=$($BINARY -t $MAINFLUX_THINGS_HOST -u $MAINFLUX_USERS_HOST -r channels get all -n $MAINFLUX_USER $TOKEN)
-BOOTSTRAP_CONFIG=$(curl -s --location --request GET $MAINFLUX_BOOTSTRAP_HOST/things/configs?name=node-red --header "Authorization: ${TOKEN}")
+namespace mainfluxBootstrap
+Log::AddOutput mainfluxBootstrap STDERR
 
-if [  $(echo $CHANNELS | jq '.total') -eq 1 ]
-then
-    # Channel exists
-    CHANNEL_ID=$(echo $CHANNELS | jq --raw-output '.channels[].id')
-    echo "Channel found with ID ${CHANNEL_ID}"
-else
-    # Channel does not exist
-    echo "No channel found. Creating..."
-    JSON_STRING='{"name":"'"$MAINFLUX_USER"'"}'
-    CHANNEL_ID=$($BINARY -t $MAINFLUX_THINGS_HOST -u $MAINFLUX_USERS_HOST -r channels create $JSON_STRING $TOKEN)
-    echo "Channel created with ID ${CHANNEL_ID}"
-fi
+try {
 
-MQTT_TOPIC="channels/${CHANNEL_ID}/messages"
 
-if [  $(echo $BOOTSTRAP_CONFIG | jq '.total') -eq 1 ]
-then
-    # Config exists
-    MQTT_USER=$(echo $BOOTSTRAP_CONFIG | jq --raw-output '.configs[].mainflux_id')
-    MQTT_PASSWORD=$(echo $BOOTSTRAP_CONFIG | jq --raw-output '.configs[].mainflux_key')
-    echo "MQTT user is ${MQTT_USER}"
-else
-    # Config does not exist
-    echo "No bootstrap config found. Creating..."
-    BS_JSON_STRING='{"external_id": "node-red", "external_key":"'"$EXTERNAL_KEY"'", "name": "node-red", "channels": ""}'
-    BS_JSON_STRING=$( jq -n \
-                  --arg ek "$EXTERNAL_KEY" \
-                  --arg ei "node-red" \
-                  --arg ch "$CHANNEL_ID" \
-                  --arg na "node-red" \
-                  '{external_id: $ei, external_key: $ek, name: $na, channels: [ $ch ]}' )
-    echo $BS_JSON_STRING
-    curl -s --request POST $MAINFLUX_BOOTSTRAP_HOST/things/configs --header "Authorization: ${TOKEN}" --header 'Content-Type: application/json' -d "${BS_JSON_STRING}"
-    BOOTSTRAP_CONFIG=$(curl -s --request GET $MAINFLUX_BOOTSTRAP_HOST/things/configs?name=node-red --header "Authorization: ${TOKEN}")
-    MQTT_USER=$(echo $BOOTSTRAP_CONFIG | jq --raw-output '.configs[].mainflux_id')
-    MQTT_PASSWORD=$(echo $BOOTSTRAP_CONFIG | jq --raw-output '.configs[].mainflux_key')
-    echo "MQTT user is ${MQTT_USER}"  
-    echo "Activating user"
-    curl -s --request POST $MAINFLUX_BOOTSTRAP_HOST/things/state/$MQTT_USER --header "Authorization: ${TOKEN}" --header 'Content-Type: application/json' -d '{"state": 1}'  
-fi
+    MAINFLUX_THINGS_HOST="${MAINFLUX_THINGS_HOST:-http://mainflux-things.mf.svc.cluster.local:8182}"
+    #MAINFLUX_THINGS_HOST="https://mainflux.k8s.h.kryno.de"
+    MAINFLUX_USERS_HOST="${MAINFLUX_USERS_HOST:-http://mainflux-users.mf.svc.cluster.local:8180}"
+    #MAINFLUX_USERS_HOST="https://mainflux.k8s.h.kryno.de"
+    MAINFLUX_BOOTSTRAP_HOST="${MAINFLUX_BOOTSTRAP_HOST:-http://mainflux-bootstrap.mf.svc.cluster.local:8182}"
+    #MAINFLUX_BOOTSTRAP_HOST="https://mainflux.k8s.h.kryno.de"
+    ENV_FILE="${ENV_FILE:-/data/env/.env}"
+    #ENV_FILE="/data/.env"
+    BINARY="/data/mainflux-cli"
 
-echo "Creating ENV file"
-echo "MQTT_USER=${MQTT_USER}" > $ENV_FILE
-echo "MQTT_PASSWORD=${MQTT_PASSWORD}" >> $ENV_FILE
-echo "MQTT_TOPIC=${MQTT_TOPIC}" >> $ENV_FILE
+    TOKEN=$($BINARY -t $MAINFLUX_THINGS_HOST -u $MAINFLUX_USERS_HOST -r users token $MAINFLUX_USER $MAINFLUX_PASSWORD)
+    NODE_RED_THINGS=$($BINARY -t $MAINFLUX_THINGS_HOST -u $MAINFLUX_USERS_HOST -r things get all -n node-red $TOKEN)
+    NODE_RED_COUNT=$(echo $NODE_RED_THINGS | jq '.total')
+    CHANNELS=$($BINARY -t $MAINFLUX_THINGS_HOST -u $MAINFLUX_USERS_HOST -r channels get all -n $MAINFLUX_USER $TOKEN)
+    BOOTSTRAP_CONFIG=$(curl -s --location --request GET $MAINFLUX_BOOTSTRAP_HOST/things/configs?name=node-red --header "Authorization: ${TOKEN}")
+
+    if [  $(echo $CHANNELS | jq '.total') -eq 1 ]
+    then
+        # Channel exists
+        CHANNEL_ID=$(echo $CHANNELS | jq --raw-output '.channels[].id')
+        Log "Channel found with ID ${CHANNEL_ID}"
+    else
+        # Channel does not exist
+        Log "No channel found. Creating..."
+        JSON_STRING='{"name":"'"$MAINFLUX_USER"'"}'
+        CHANNEL_ID=$($BINARY -t $MAINFLUX_THINGS_HOST -u $MAINFLUX_USERS_HOST -r channels create $JSON_STRING $TOKEN)
+        Log "$(UI.Color.Green)Channel created with ID ${CHANNEL_ID}$(UI.Color.Default)"
+    fi
+
+    MQTT_TOPIC="channels/${CHANNEL_ID}/messages"
+
+    if [  $(echo $BOOTSTRAP_CONFIG | jq '.total') -eq 1 ]
+    then
+        # Config exists
+        MQTT_USER=$(echo $BOOTSTRAP_CONFIG | jq --raw-output '.configs[].mainflux_id')
+        MQTT_PASSWORD=$(echo $BOOTSTRAP_CONFIG | jq --raw-output '.configs[].mainflux_key')
+        Log "MQTT user is ${MQTT_USER}"
+    else
+        # Config does not exist
+        Log "No bootstrap config found. Creating..."
+        BS_JSON_STRING='{"external_id": "node-red", "external_key":"'"$EXTERNAL_KEY"'", "name": "node-red", "channels": ""}'
+        BS_JSON_STRING=$( jq -n \
+                    --arg ek "$EXTERNAL_KEY" \
+                    --arg ei "node-red" \
+                    --arg ch "$CHANNEL_ID" \
+                    --arg na "node-red" \
+                    '{external_id: $ei, external_key: $ek, name: $na, channels: [ $ch ]}' )
+        curl -s --request POST $MAINFLUX_BOOTSTRAP_HOST/things/configs --header "Authorization: ${TOKEN}" --header 'Content-Type: application/json' -d "${BS_JSON_STRING}"
+        BOOTSTRAP_CONFIG=$(curl -s --request GET $MAINFLUX_BOOTSTRAP_HOST/things/configs?name=node-red --header "Authorization: ${TOKEN}")
+        MQTT_USER=$(echo $BOOTSTRAP_CONFIG | jq --raw-output '.configs[].mainflux_id')
+        MQTT_PASSWORD=$(echo $BOOTSTRAP_CONFIG | jq --raw-output '.configs[].mainflux_key')
+        Log "$(UI.Color.Green)MQTT user is ${MQTT_USER}$(UI.Color.Default)"  
+        Log "Activating user"
+        curl -s --request POST $MAINFLUX_BOOTSTRAP_HOST/things/state/$MQTT_USER --header "Authorization: ${TOKEN}" --header 'Content-Type: application/json' -d '{"state": 1}'  
+    fi
+
+    Log "Creating ENV file"
+    echo "MQTT_USER=${MQTT_USER}" > $ENV_FILE
+    echo "MQTT_PASSWORD=${MQTT_PASSWORD}" >> $ENV_FILE
+    echo "MQTT_TOPIC_TELE=${MQTT_TOPIC}/tele/#" >> $ENV_FILE
+    echo "MQTT_TOPIC_STAT=${MQTT_TOPIC}/stat/#" >> $ENV_FILE
+} catch {
+    Log "Could not bootstrap Node-Red Thing in Mainflux!"
+    Log "Caught Exception:$(UI.Color.Red) $__BACKTRACE_COMMAND__ $(UI.Color.Default)"
+    Log "File: $__BACKTRACE_SOURCE__, Line: $__BACKTRACE_LINE__"
+
+    ## printing a caught exception couldn't be simpler, as it's stored in "${__EXCEPTION__[@]}"
+    Exception::PrintException "${__EXCEPTION__[@]}"
+    exit 1
+}
+
 
 
