a2b297a81ebd352aac101c1657c914a712c8ab11
Jan <jan@kryno.de>
2023-06-12 11:49:36 +0000

    init emqx

--- Dockerfile
+++ Dockerfile
@@ -1,42 +1,11 @@
-FROM golang:1.19-alpine AS builder
-ARG SVC
-ARG GOARCH
-ARG GOARM
-ARG VERSION
-ARG COMMIT
-ARG TIME
-ARG TARGETPLATFORM
-
-# Set the working directory
-WORKDIR /app
-
-# Clone the Mainflux repository
-RUN apk --no-cache add git
-RUN git clone https://github.com/mainflux/mainflux.git .
-RUN git checkout $VERSION
-
-RUN case $TARGETPLATFORM in \
-    "linux/amd64") GOARCH="amd64" ;; \
-    "linux/arm64") GOARCH="arm64" ;; \
-    *) GOARCH="amd64" ;; \
-    esac
-
-# Build the Mainflux CLI service with the appropriate GOARCH
-RUN CGO_ENABLED=0 GOARCH=$GOARCH go build -o mainflux-cli ./cmd/cli
-
-
-FROM alpine
+FROM alpine/httpie
 ENV TERM xterm-256color
-RUN apk --no-cache add bash curl jq \
-    && mkdir /data
+
 # Set the working directory
 WORKDIR /data    
 
-# Copy the Mainflux CLI service based on the host architecture
-COPY --from=builder /app/mainflux-cli ./mainflux-cli
-
-COPY bootstrap_mainflux.sh .
+COPY bootstrap_emqx.sh .
 COPY lib ./lib/
 
-ENTRYPOINT [ "./bootstrap_mainflux.sh" ]
+ENTRYPOINT [ "./bootstrap_emqx.sh" ]
 
---
+++ bootstrap_emqx.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+# Set the script execution environment
+TERM=xterm-256color
+
+# Import necessary libraries
+source "$(cd "${BASH_SOURCE[0]%/*}" && pwd)/lib/oo-bootstrap.sh"
+import util/log util/exception util/tryCatch
+
+# Set the log output
+namespace emqxBootstrap
+Log::AddOutput emqxBootstrap STDERR
+
+# Define the Mainflux hosts and other variables
+EMQX_HOST="${EMQX_HOST:-emqx-dashboard.emqx.svc.cluster.local:18083}"
+EMQX_API_KEY="${EMQX_API_KEY}"
+EMQX_API_USER="${EMQX_API_USER:-bootstrap_node_red}"
+CHANNEL_ID="${CHANNEL_ID:-3c78cf3f-d5b5-40ed-b851-bd86c2edaa52}"
+MQTT_USER="${MQTT_USER:-node-red}"
+MQTT_PASSWORD="${MQTT_PASSWORD}"
+
+ENV_FILE="${ENV_FILE:-/data/env/.env}"
+
+create_env_file() {
+    # Create the ENV file
+    MQTT_ROOT_TOPIC="channels/$CHANNEL_ID/messages"
+    Log "Creating ENV file"
+    echo "MQTT_USER=$MQTT_USER" >"$ENV_FILE"
+    echo "MQTT_PASSWORD=$MQTT_PASSWORD" >>"$ENV_FILE"
+    echo "MQTT_ROOT_TOPIC=$MQTT_ROOT_TOPIC" >>"$ENV_FILE"
+    echo "MQTT_TOPIC_TELE=$MQTT_ROOT_TOPIC/tele/#" >>"$ENV_FILE"
+    echo "MQTT_TOPIC_STAT=$MQTT_ROOT_TOPIC/stat/#" >>"$ENV_FILE"
+    echo "MQTT_TOPIC_CMND=$MQTT_ROOT_TOPIC/cmnd/" >>"$ENV_FILE"
+}
+
+
+# Step 1: Check if user 'node-red' exists
+EXISTING_USER=$(http --auth "$EMQX_API_USER:$EMQX_API_KEY" --ignore-stdin --check-status GET http://$EMQX_HOST/api/v5/authentication/password_based:built_in_database/users/node-red &> /dev/null; echo $?)
+
+if [[ $EXISTING_USER -eq 0 ]]; then
+  Log "User 'node-red' already exists."
+  create_env_file
+  exit 0
+fi
+
+# Step 2: Create user 'node-red'
+
+RESPONSE=$(http --auth "$EMQX_API_USER:$EMQX_API_KEY" --ignore-stdin POST http://$EMQX_HOST/api/v5/authentication/password_based:built_in_database/users \
+  username="$MQTT_USER" \
+  password="$MQTT_PASSWORD" \
+  is_superuser=false)
+
+CREATION_STATUS=$(echo "$RESPONSE" | grep -o -m 1 '"status": "[^"]*' | cut -d'"' -f4)
+
+if [[ $CREATION_STATUS == "success" ]]; then
+  Log "User 'node-red' created successfully."
+  create_env_file
+  exit 0
+else
+  ERROR_MSG=$(echo "$RESPONSE" | grep -o -m 1 '"error": "[^"]*' | cut -d'"' -f4)
+  { e="$ERROR_MSG"; throw; }
+  Log "Caught Exception:$(UI.Color.Red) $__BACKTRACE_COMMAND__ $(UI.Color.Default)"
+  Log "File: $__BACKTRACE_SOURCE__, Line: $__BACKTRACE_LINE__"
+  Exception::PrintException "${__EXCEPTION__[@]}"
+  exit 1
+fi
+
+
+
+
+
+
+
+
+
