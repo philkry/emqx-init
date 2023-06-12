8140caa4bdc1bdeb9c56a13b277bde2dda915c04
Jan <jan@kryno.de>
2023-06-12 13:58:07 +0000

    sdfdsf

--- bootstrap_emqx.sh
+++ bootstrap_emqx.sh
@@ -35,7 +35,7 @@ EOF
 
 
 # Step 1: Check if user 'node-red' exists
-EXISTING_USER=$(http --auth "$EMQX_API_USER:$EMQX_API_KEY" --ignore-stdin --check-status GET http://$EMQX_HOST/api/v5/authentication/password_based:built_in_database/users/node-red &> /dev/null; echo $?)
+EXISTING_USER=$(http --auth "$EMQX_API_USER:$EMQX_API_KEY" --ignore-stdin --check-status GET "http://$EMQX_HOST/api/v5/authentication/password_based:built_in_database/users/node-red" &> /dev/null; echo $?)
 
 if [[ $EXISTING_USER -eq 0 ]]; then
   echo "User 'node-red' already exists."
@@ -45,7 +45,7 @@ fi
 
 # Step 2: Create user 'node-red'
 
-RESPONSE=$(http --auth "$EMQX_API_USER:$EMQX_API_KEY" POST http://$EMQX_HOST/api/v5/authentication/password_based:built_in_database/users user_id=$MQTT_USER password=$MQTT_PASSWORD)
+RESPONSE=$(http --auth "$EMQX_API_USER:$EMQX_API_KEY" POST "http://$EMQX_HOST/api/v5/authentication/password_based:built_in_database/users" user_id=$MQTT_USER password=$MQTT_PASSWORD)
 
 CREATION_STATUS=$(echo "$RESPONSE" | grep -o -m 1 '"status": "[^"]*' | cut -d'"' -f4)
 
