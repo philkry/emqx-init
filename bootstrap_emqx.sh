93403445594a87c9bc1791a5663c94b59b432990
philkry <philkry@users.noreply.github.com>
2024-08-23 09:43:21 +0200

    Update bootstrap_emqx.sh

--- bootstrap_emqx.sh
+++ bootstrap_emqx.sh
@@ -34,9 +34,9 @@ create_authorization() {
         [0][rules][0][permission]=allow \
         [0][rules][0][topic]="$MQTT_TOPIC/#") 
 
-    AUTH_STATUS=$(echo "$RESPONSE" | grep -o -m 1 '"status": "[^"]*' | cut -d'"' -f4)
+    AUTH_STATUS=$(echo "$RESPONSE" | grep -o -m 1 '"code": "[^"]*' | cut -d'"' -f4 | tr '[:upper:]' '[:lower:]')
 
-    if [[ $AUTH_STATUS == "success" ]]; then
+    if [[ $AUTH_STATUS == "success" || $AUTH_STATUS == "already_exists" ]]; then
         echo "Authorization for topic '$MQTT_TOPIC' created successfully."
     else
         ERROR_MSG=$(echo "$RESPONSE" | grep -o -m 1 '"error": "[^"]*' | cut -d'"' -f4)
