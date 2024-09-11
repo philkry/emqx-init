94d768ca09195b05115b94e6771ce085862fa2d0
Jan <philkry@gmx.de>
2024-09-11 17:56:14 +0200

    parsing headers correctly

--- bootstrap_emqx.sh
+++ bootstrap_emqx.sh
@@ -2,7 +2,7 @@
 
 # Set the script execution environment
 TERM=xterm-256color
-set -x
+#set -x
 
 # Define the Mainflux hosts and other variables
 EMQX_HOST="${EMQX_HOST:-emqx-dashboard.emqx.svc.cluster.local:18083}"
@@ -15,15 +15,15 @@ MQTT_TOPIC="channels/$CHANNEL_ID/messages"
 create_authorization() {
     local username="$1"
     # Create authorization for the MQTT_TOPIC
-    RESPONSE=$(http --ignore-stdin --auth "$EMQX_API_USER:$EMQX_API_KEY" POST "http://$EMQX_HOST/api/v5/authorization/sources/built_in_database/rules/users" \
+    RESPONSE=$(http -h --ignore-stdin --auth "$EMQX_API_USER:$EMQX_API_KEY" POST "http://$EMQX_HOST/api/v5/authorization/sources/built_in_database/rules/users" \
         [0][username]="$username" \
         [0][rules][0][action]=all \
         [0][rules][0][permission]=allow \
-        [0][rules][0][topic]="$MQTT_TOPIC/#") 
+        [0][rules][0][topic]="$MQTT_TOPIC/#")
 
-    AUTH_STATUS=$(echo "$RESPONSE" | grep -o -m 1 '"code":"[^"]*' | cut -d'"' -f4 | tr '[:upper:]' '[:lower:]')
+    HTTP_STATUS=$(echo "$RESPONSE" | head -n 1 | cut -d' ' -f2)
 
-    if [[ $AUTH_STATUS == "success" || $AUTH_STATUS == "already_exists" ]]; then
+    if [[ $HTTP_STATUS -eq 200 || $HTTP_STATUS -eq 201 ]]; then
         echo "Authorization for user '$username' and topic '$MQTT_TOPIC' created successfully."
     else
         ERROR_MSG=$(echo "$RESPONSE" | grep -o -m 1 '"message":"[^"]*' | cut -d'"' -f4)
@@ -36,23 +36,26 @@ create_user() {
     local password="$2"
 
     # Check if user exists
-    EXISTING_USER=$(http --auth "$EMQX_API_USER:$EMQX_API_KEY" --ignore-stdin --check-status GET "http://$EMQX_HOST/api/v5/authentication/password_based:built_in_database/users/$username" &> /dev/null; echo $?)
+    RESPONSE=$(http -h --auth "$EMQX_API_USER:$EMQX_API_KEY" --ignore-stdin GET "http://$EMQX_HOST/api/v5/authentication/password_based:built_in_database/users/$username")
+    HTTP_STATUS=$(echo "$RESPONSE" | head -n 1 | cut -d' ' -f2)
 
-    if [[ $EXISTING_USER -eq 0 ]]; then
+    if [[ $HTTP_STATUS -eq 200 ]]; then
         echo "User '$username' already exists."
-    else
+    elif [[ $HTTP_STATUS -eq 404 ]]; then
         # Create user
-        RESPONSE=$(http --ignore-stdin --auth "$EMQX_API_USER:$EMQX_API_KEY" POST "http://$EMQX_HOST/api/v5/authentication/password_based:built_in_database/users" user_id="$username" password="$password")
+        RESPONSE=$(http -h --ignore-stdin --auth "$EMQX_API_USER:$EMQX_API_KEY" POST "http://$EMQX_HOST/api/v5/authentication/password_based:built_in_database/users" user_id="$username" password="$password")
+        HTTP_STATUS=$(echo "$RESPONSE" | head -n 1 | cut -d' ' -f2)
 
-        CREATION_STATUS=$(echo "$RESPONSE" | grep -o -m 1 '"status": "[^"]*' | cut -d'"' -f4)
-
-        if [[ $CREATION_STATUS == "success" ]]; then
+        if [[ $HTTP_STATUS -eq 201 ]]; then
             echo "User '$username' created successfully."
         else
-            ERROR_MSG=$(echo "$RESPONSE" | grep -o -m 1 '"error": "[^"]*' | cut -d'"' -f4)
+            ERROR_MSG=$(echo "$RESPONSE" | grep -o -m 1 '"message":"[^"]*' | cut -d'"' -f4)
             echo "Failed to create user '$username': $ERROR_MSG"
             return 1
         fi
+    else
+        echo "Unexpected response when checking user '$username': HTTP $HTTP_STATUS"
+        return 1
     fi
 
     # Create authorization for the user
